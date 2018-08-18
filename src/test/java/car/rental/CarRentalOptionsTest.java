3b4eb303b3267957123731dc3f6d0a0930b13a99
Piotr Janik <must.janik@gmail.com>
2018-08-18 12:59:54 +0200

    Removed unnecessary things, added test methods

--- src/test/java/car/rental/CarRentalOptionsTest.java
+++ src/test/java/car/rental/CarRentalOptionsTest.java
@@ -1,6 +1,8 @@
 package car.rental;
 
+import car.rental.model.Car;
 import car.rental.model.Client;
+import car.rental.model.RentingACar;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -12,7 +14,9 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class CarRentalOptionsTest {
@@ -21,18 +25,73 @@ public class CarRentalOptionsTest {
     private CarRentalStorage carRentalStorageMock;
     private CarRentalOptions carRentalOptions;
     private Client client;
+    private Car car;
+    private RentingACar rentingACar;
 
     @Before
     public void setup() {
         MockitoAnnotations.initMocks(this);
+        carRentalOptions = new CarRentalOptions(carRentalStorageMock);
     }
 
     @Test
     public void createNewCustomer() throws SQLException {
-        carRentalOptions = new CarRentalOptions(carRentalStorageMock);
         client = new Client();
         carRentalOptions.createNewCustomer(client);
 
         verify(carRentalStorageMock).addClient(client);
+    }
+
+    @Test
+    public void returnACar() throws SQLException {
+        car = new Car();
+        carRentalOptions.returnACar(car);
+
+        verify(carRentalStorageMock).returnACar(car);
+    }
+
+
+    @Test
+    public void createNewCar() throws SQLException {
+        car = new Car();
+        carRentalOptions.createNewCar(car);
+
+        verify(carRentalStorageMock).addNewCar(car);
+    }
+
+
+    @Test
+    public void makeCarUnavailable() throws SQLException {
+        car = new Car();
+
+        carRentalOptions.makeCarUnavailable(car);
+        verify(carRentalStorageMock).makeCarUnavailable(car);
+    }
+
+    @Test
+    public void makeCarAvailable() throws SQLException {
+        car = new Car();
+
+        carRentalOptions.makeCarAvailable(car);
+        verify(carRentalStorageMock).makeCarAvailable(car);
+    }
+
+    @Test
+    public void rentACar() throws SQLException {
+        rentingACar = new RentingACar();
+        carRentalOptions.rentACar(rentingACar);
+
+        verify(carRentalStorageMock).rentACar(rentingACar);
+    }
+
+    @Test
+    public void findAllCustomers() throws SQLException
+    {
+        client = new Client();
+        List<Client> listOfClients = new ArrayList<Client>();
+        listOfClients.add(client);
+        when(carRentalStorageMock.getAllCustomers()).thenReturn(listOfClients);
+
+        verify(carRentalStorageMock).getAllCustomers();
     }
 }
